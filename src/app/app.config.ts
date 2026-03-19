466272ddd9c29a157080bdb4e6fba9771b23f860
Gezino Linden <Sibabalwel@lumoka.co.za>
2026-03-19 12:15:08 +0200

    fix: use withInMemoryScrolling for scroll restoration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 ﻿import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withRouterConfig } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
@@ -10,9 +10,10 @@ import { errorInterceptor } from './interceptors/error.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withRouterConfig({scrollPositionRestoration: 'top'})),
+    provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: 'top'})),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     provideCharts(withDefaultRegisterables()),
   ],
 };
+
 
